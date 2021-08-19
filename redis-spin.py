ef5b71cd8011f335d58b26e1661d3f029b3dac8c
Anas DADI <dadi.anas@hotmail.fr>
2021-08-19 16:44:32 +0100

    fix bug mount volume + config

--- redis-spin.py
+++ redis-spin.py
@@ -43,6 +43,39 @@ def generate_production_redis_spin():
     return redis_dockercompose
 
 
+def generate_production_redis_spin2():
+    redis_dockercompose = DockerCompose(file_name="docker-compose.yml", version="2")
+    redis_dockercompose.add_network("redis-network", {"driver": "bridge"})
+
+    for i in range(3):
+        port = str(7000 + i)
+        redis_service = Service(image="redis:alpine",
+                                container_name="redis-" + port,
+                                networks=["redis-network"],
+                                ports=[port + ":" + port],
+                                volumes=["./production/generated_config/:/usr/local/etc/redis/"],
+                                commands="redis-server /usr/local/etc/redis/redis" + port + ".conf --requirepass dadianas")
+        redis_dockercompose.add_service("redis-" + port, redis_service)
+        with open("./production/slave_config/redis.conf", 'r') as slave_config:
+            with open("./production/generated_config/redis"+port+".conf", 'w') as current_master_config:
+                current_master_config.write("\n".join(slave_config.readlines())+"\nport " + port)
+
+    for i in range(3, 6):
+        port = str(7000 + i)
+        redis_service = Service(image="redis:alpine",
+                                container_name="redis-" + port,
+                                networks=["redis-network"],
+                                ports=[port + ":" + port],
+                                volumes=["./production/generated_config/:/usr/local/etc/redis/"],
+                                commands="redis-server /usr/local/etc/redis/redis" + port + ".conf --requirepass dadianas"
+                                )
+        redis_dockercompose.add_service("redis-" + port, redis_service)
+        with open("./production/master_config/redis.conf", 'r') as slave_config:
+            with open("./production/generated_config/redis"+port+".conf", 'w') as current_slave_config:
+                current_slave_config.write('\n'.join(slave_config.readlines())+"\nport " + port)
+    return redis_dockercompose
+
+
 def log_time(message):
     """
         This function helps to add time to log message
@@ -58,14 +91,15 @@ if __name__ == '__main__':
             chdir(getcwd() + "/development")
             system("docker-compose up ")
         elif argv[1] == 'prod':
-            generate_production_redis_spin().to_yaml(path="./production/")
+            generate_production_redis_spin2().to_yaml(path="./production/")
             chdir(getcwd() + "/production")
             system("docker-compose up ")
-    except Exception:
+    except :
         warning_message = """
         To use this script you should follow the following pattern:
         redis-spin.py <mode>
         where mode can be dev or prod.
         """
+        print(warning_message)
         logging.warning(log_time(warning_message))
-        raise Exception
+
